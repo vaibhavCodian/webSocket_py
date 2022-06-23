c9c44060a747c83cc5be655531d5af8e47d1bcc7
Vaibhav <vaibhavshukla.try@gmail.com>
2022-06-23 22:19:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import os
 from flask import Flask
+from flask_sock import Sock
+
 app = Flask(__name__)
+app.config['SECRET'] = "bruh"
+sock = Sock(app)
+
 
 @app.route("/")
 def hello():
     return "Hello from Python!"
+
+
+@sock.route('/echo')
+def echo(ws):
+    while True:
+        data = ws.receive()
+        ws.send(data)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
