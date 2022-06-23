c779eb285c18c63455787be79bacd3dae2a9fc5d
Vaibhav <vaibhavshukla.try@gmail.com>
2022-06-23 21:39:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,11 @@
-# import os
-# from flask import Flask
-# app = Flask(__name__)
-#
-# @app.route("/")
-# def hello():
-#     return "Hello from Python!"
-#
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
-
-import asyncio
 import os
+from flask import Flask
+app = Flask(__name__)
 
-import websockets
+@app.route("/")
+def hello():
+    return "Hello from Python!"
 
-connected = set()
-
-
-async def server(websocket, path):
-    # Register
-    connected.add(websocket)
-    try:
-        async for message in websocket:
-            for conn in connected:
-                if conn != websocket:
-                    await conn.send(f'Got a new Masssage: {message}')
-    finally:
-        connected.remove(websocket)
-
-port = int(os.environ.get('PORT', 17995))
-start_server = websockets.serve(server, '0.0.0.0', port)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
